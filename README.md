945dbfe85c0edcad0e993235c333c978ed0f6821
likun <285781031@qq.com>
2021-03-15 16:40:47 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hello
+1px -- 2rpx   ; 10px -- 20rpx   响应式单位；
 
---
+++ miniprogram/components/navbar/navbar.js
@@ -0,0 +1,22 @@
+// components/navbar/navbar.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    barList:[{name:"首页",src:""},{name:"圈子",src:""},{name:"我的",src:""}]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
